6f3d808c8ec95a11999346800e982f2126577673
dedmasai <114438951+dedmasai@users.noreply.github.com>
2022-09-28 00:33:21 +0300

    init

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyChs')
+d=4+2
+
+def lod(d):
+    return d
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
