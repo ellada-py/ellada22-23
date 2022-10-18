73d9e178471078083a0e3f47cf6d9f32b0137df6
sonia <dedmasai@gmail.com>
2022-10-18 14:13:47 +0300

    соня задания

---
+++ 11/sofa/10.py
@@ -0,0 +1,9 @@
+a=int(input('число а: '))
+b=int(input('число b: '))
+import math
+print('сумма чисел: '+str(a+b))
+print('разница между числами: '+str(a-b))
+print('частное от деления a на b: '+str(a//b))
+print('остаток от деления a на b: '+str(a%b))
+print('десятичный логарифм числа a: '+str(math.log10(a)))
+print('число a в степени b: '+str(a**b))
