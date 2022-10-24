4469bd223830be62599d1998d5417766587bcffa
Ученик <llkj@yujgt>
2022-10-24 09:31:37 +0300

    nm

---
+++ 10/Tatiana/38.py
@@ -0,0 +1,19 @@
+a = int(input("количество сторон: "))
+if a = 3:
+    print("треугольник")
+elif a = 4:
+    print("")
+elif a = 5:
+    print("")
+elif a = 6:
+    print("")
+elif a = 7:
+    print("")
+elif a = 8:
+    print("")
+elif a = 9:
+    print("")
+elif a = 10:
+    print("")
+else:
+    no
