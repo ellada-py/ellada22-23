27b62b23494a04e0fe3f8fcabde8f2efa9ad6224
Программист 1 <kljh@l;iju>
2022-10-25 09:41:12 +0300

    46

--- 10/Alexey/46.py
+++ 10/Alexey/46.py
@@ -1,6 +1,6 @@
 a = input('Введите букву: ')
 b = int(input('Введите цифру: '))
-if a == 'a' or 'c' or 'e' or 'g':
+if a == 'a' or a == 'c' or a == 'e' or a == 'g':
     if b % 2 == 0:
         print('Белая')
     else:
