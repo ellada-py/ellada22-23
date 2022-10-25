eb64b2089c1d5de17f138fda257c737764ef4988
Программист 1 <kljh@l;iju>
2022-10-25 09:41:12 +0300

    56

---
+++ 10/Alexey/52.py
@@ -0,0 +1,27 @@
+mark = input("Введите буквенную оценку (от A до F): ")
+if mark == "A" or mark == "A+":
+    print("Оценка: 4,0")
+if mark == "A-":
+    print("Оценка: 3,7")
+if mark == "B+":
+    print("Оценка: 3,3")
+if mark == "B+":
+    print("Оценка: 3,3")
+if mark == "B":
+    print("Оценка: 3,0")
+if mark == "B-":
+    print("Оценка: 2,7")
+if mark == "C+":
+    print("Оценка: 2,3")
+if mark == "C":
+    print("Оценка: 2,0")
+if mark == "C-":
+    print("Оценка: 1,7")
+if mark == "D+":
+    print("Оценка: 1,3")
+if mark == "D":
+    print("Оценка: 1,0")
+if mark == "F":
+    print("Оценка: 0")
+if mark != "A" and mark != "A-" and mark != "B+" and mark != "B" and mark != "B-" and mark != "C+" and mark != "C" and mark != "C-" and mark != "D+" and mark != "D" and mark != "F":
+    print("Ошибка: введите коректную букву!")
