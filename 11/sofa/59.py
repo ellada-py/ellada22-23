80e3d8c751ca7eeeb686185b65b4328155e5a26b
123 <dd@ddd>
2022-10-25 12:57:00 +0300

    задания

--- 11/sofa/59.py
+++ 11/sofa/59.py
@@ -4,14 +4,56 @@ probel=data.find(' ')
 probel2=data.rfind(' ')
 day=int(data[:probel])
 month=data[probel+1:probel2]
-year=data[probel2+1:]
-if (month=='января') or (month=='марта') or (month=='мая') or (month=='июля') or (month=='августа') or (month=='октября') or (month=='декабря'):
+year=int(data[probel2+1:])
+if (month=='января') or (month=='марта') or (month=='мая') or (month=='июля') or (month=='августа') or (month=='октября'):
     if day<31:
         day+=1
     elif day==31:
         day=1
-        n=m.find(month)
+        n=m.index(month)
         month=m[n+1]
-print(day)
-print(month)
-print(m[1])
+elif month=='февраля':
+    if year % 400 == 0:
+        if day<29:
+            day+=1
+        else:
+            day=1
+            n=m.index(month)
+            month=m[n+1]
+    elif year % 100 == 0:
+        if day < 28:
+            day += 1
+        else:
+            day = 1
+            n = m.index(month)
+            month = m[n + 1]
+    elif year % 4 == 0:
+        if day < 29:
+            day += 1
+        else:
+            day = 1
+            n = m.index(month)
+            month = m[n + 1]
+    else:
+        if day < 28:
+            day += 1
+        else:
+            day = 1
+            n = m.index(month)
+            month = m[n + 1]
+elif month=='декабря':
+    if day < 31:
+        day += 1
+    elif day == 31:
+        day = 1
+        n = m.index(month)
+        month = m[n + 1]
+        year+=1
+else:
+    if day < 30:
+        day += 1
+    elif day == 30:
+        day = 1
+        n = m.index(month)
+        month = m[n + 1]
+print('следующий день: '+str(day)+' '+month+' '+str(year))
