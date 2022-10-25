8b32af62335bcc5afa86cbc8ff942d8319700b0b
sonia <dedmasai@gmail.com>
2022-10-25 09:41:05 +0300

    vz

--- 10/Tatiana/52.py
+++ 10/Tatiana/52.py
@@ -1,6 +1,11 @@
 a = int(input("буквенная оценка: "))
 b = int(input("цифровая оценка: "))
-if a = ("A"):
-    print("4")
-while A+ B+ C+ D+ :
-    sum+=0.3
+if == 'A+':
+    print('4')
+elif == 'A':
+    print('4')
+elif == 'A-':
+    print('3.7')
+elif == 'B+':
+    print('3.3')
+    elif ==
