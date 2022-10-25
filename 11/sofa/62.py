0d3682e898df41498435c35dcccfb9ddd7a8bff2
123 <dd@ddd>
2022-10-25 13:53:55 +0300

    задания

--- 11/sofa/62.py
+++ 11/sofa/62.py
@@ -1,11 +1,9 @@
 import random
-red=(1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36)
 nomber=int(random.choice([0,00,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38]))
 print('выпавший номер: '+str(nomber))
 print('выигравшая ставка: '+str(nomber))
 if (nomber!=0) or (nomber!=00):
-    n=red.find(nomber)+1
-    if nomber==red[n]:
+    if (nomber==1) or (nomber==3) or (nomber==5) or (nomber==7) or (nomber==9) or (nomber==12) or (nomber==14) or (nomber==16) or (nomber==18) or (nomber==19) or (nomber==21) or (nomber==21) or (nomber==23) or (nomber==25) or (nomber==27) or (nomber==30) or (nomber==32) or (nomber==34) or (nomber==36):
         print('выигравшая ставка: красное')
         if nomber%2:
             print('выигравшая ставка: нечетное')
