51f53fda9be527c47185d024abccaf5bf07d9af9
Программист 4 <hkjh@jlkj>
2022-10-28 12:35:05 +0300

    заданияhggujhk

--- 11/sofa/121.py
+++ 11/sofa/121.py
@@ -3,11 +3,8 @@ number=random.randint(1,49)
 ticket=[number]
 while len(ticket)!=6:
    for i in range(len(ticket)):
-       number=random.randint(1,49)
-       if number==ticket[i]:
+        if number==ticket[i]:
            number=random.randint(1,49)
-       else:
-           ticket.append(number)
-           number=random.randint(1,49)
+        ticket.append(number)
 ticket.sort()
 print(list(map(str,ticket)))
