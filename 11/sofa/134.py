24e3163c5b4fb9de3caf85e46a2052f79a0a8642
sofa <ellada.py@mail.ru>
2022-10-30 21:51:48 +0300

    задания

--- 11/sofa/134.py
+++ 11/sofa/134.py
@@ -1,11 +1,6 @@
 numbers=input('введите числа через пробел: ').split(' ')
 list=[[]]
 k=0
-while k!=len(numbers):
-
-    for i in range(k+1):
-        list+=numbers[i:k+i]
-    k+=1
-print(list)
+for i in range()
 
 
