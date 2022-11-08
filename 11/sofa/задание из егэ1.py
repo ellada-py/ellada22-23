a3d38239799395e1b99ca58753302a6e5345b16c
123 <dd@ddd>
2022-11-08 13:59:44 +0300

    yjyjyjed

--- 11/sofa/задание из егэ1.py
+++ 11/sofa/задание из егэ1.py
@@ -2,5 +2,16 @@ def m(n):
     max1 = 0
     max2 = 0
     for i in range(2,n):
-        if n%i==0
+        if n%i==0:
+            k=i
             max1=n/i
+            break
+    for j in range (i+1,n):
+        if n%j==0:
+            max2=n/j
+            break
+    sum=max1+max2
+    return sum
+number=11000001
+
+print(m(600))
