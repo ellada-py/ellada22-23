358d6f7188fd50bf412751d6a3145d01e3c6d632
123 <dd@ddd>
2022-11-08 14:10:09 +0300

    yjyjyjed

--- 11/sofa/задание из егэ1.py
+++ 11/sofa/задание из егэ1.py
@@ -5,13 +5,21 @@ def m(n):
         if n%i==0:
             k=i
             max1=n/i
-            break
+        break
     for j in range (i+1,n):
         if n%j==0:
             max2=n/j
-            break
+        break
     sum=max1+max2
     return sum
 number=11000001
-
-print(m(600))
+l=0
+spisok=[]
+while l!=5:
+    if 0<m(number)<10000:
+        l+=1
+        spisok.append(number)
+        number+=1
+    else:
+        number+=1
+print(spisok)
