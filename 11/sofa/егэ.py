4347c8d2d8ae1b2c0c76219ce404d689f50d8cc7
123 <dd@ddd>
2022-11-22 14:08:49 +0300

    yjyjyjed

--- 11/sofa/егэ.py
+++ 11/sofa/егэ.py
@@ -1,10 +1,7 @@
-d=[]
 for i in range (35000000,40000001):
+    d=[]
     for j in range(1,i+1):
-        if i%j:
+        if (j%2==1) and(i%j):
             d.append(j)
-    for l in range (len(d)):
-        if d[l]%2:
-            p=d.pop(l)
     if len(d)==5:
         print(i)
