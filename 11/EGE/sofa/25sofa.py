e13e073fa85aee84fdb710f2d9bbb3bbb4b2c92f
123 <dd@ddd>
2022-11-29 13:12:47 +0300

    yjyjyjed

--- 11/EGE/sofa/25sofa.py
+++ 11/EGE/sofa/25sofa.py
@@ -1,7 +1,22 @@
 for i in range(45000000,50000001):
-    count=0
     if i % 2 == 1:
         count = 1
+        sqrtI = round(i ** 0.5)
+        for j in range(2, sqrtI + 1):
+            if i % j == 0:
+                if j % 2 == 1:
+                    count += 1
+                k = i // j
+                if k % 2 == 1:
+                    count += 1
+                    if j == k:
+                        count -= 1
+                if count > 4:
+                    break
+        if count == 4:
+            print(i)
+    else:
+        count=0
         sqrtI = round(i ** 0.5)
         for j in range(2, sqrtI + 1):
             if i % j == 0:
