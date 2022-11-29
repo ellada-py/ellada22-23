a761c6556c526b6b705036e74c1fe8393f7bd0fa
123 <dd@ddd>
2022-11-29 13:15:07 +0300

    yjyjyjed

--- 11/EGE/sofa/25sofa.py
+++ 11/EGE/sofa/25sofa.py
@@ -1,8 +1,8 @@
 for i in range(45000000,50000001):
     if i % 2 == 1:
-        count = 1
+        count = 2
     else:
-        count=0
+        count=1
     sqrtI = round(i ** 0.5)
     for j in range(2, sqrtI + 1):
         if i % j == 0:
