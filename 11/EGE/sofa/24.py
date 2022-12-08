9b4eaf17faac2b59d5b18aacfc65cf8df3be5d6b
sofa <ellada.py@mail.ru>
2022-12-08 22:39:31 +0300

    задания

--- 11/EGE/sofa/24.py
+++ 11/EGE/sofa/24.py
@@ -1,14 +1,12 @@
 f=open('24.txt')
 s=f.read()
-max1=1
-max2=0
-for i in range (len(s)-1):
-    if s[i]!='P':
-        max2+=1
-    elif s[i]=='P' and s[i+1]!='P':
-        max2+=1
+k=1
+max=0
+for i in range(1, len(s)):
+    if s[i] == 'P' and s[i-1] == 'P':
+        k = 1
     else:
-        if max2>max1:
-            max1=max2
-            max2=0
-print(max1)
+        k += 1
+        if k > max:
+            max = k
+print(max)
---
+++ 11/EGE/sofa/27.py
@@ -0,0 +1,11 @@
+f=open('27_A.txt')
+s=[int(i) for i in f]
+d=160
+p=7
+max=0
+for i in range(1,s[0]):
+    if (s[i]%d!=s[i+1]%d) and ((s[i]%p==0) or (s[i+1]%p==0)):
+        if s[i]+s[i+1]>max:
+            max=s[i]+s[i+1]
+            para=[s[i],s[i+1]]
+print(para)
