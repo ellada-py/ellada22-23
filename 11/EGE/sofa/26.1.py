b9730eaadd99e85d46f84d97d6a3c4ce64d5ccc0
123 <dd@ddd>
2023-01-10 13:34:13 +0300

    kulfuykjklnljhgv;k/nbjlghkfjghj

--- 11/EGE/sofa/26.1.py
+++ 11/EGE/sofa/26.1.py
@@ -1,12 +1,2 @@
-f = open('26.txt')
-s, n = map(int, f.readline().split())
-v = sorted(map(int, f))
-sum, cnt = 0, 0
-for i in range(len(v)):
-    if sum + v[i] <= s:
-        sum += v[i]
-        cnt += 1
-biggest_file = v[cnt - 1:][0] + s - sum
-while biggest_file not in v:
-    biggest_file -= 1
-print(cnt, biggest_file)
+f=open('27883.txt'):
+s=[]
--- 11/EGE/sofa/ююю.py
+++ 11/EGE/sofa/ююю.py
@@ -4,8 +4,13 @@ for i in range (174457,174506):
     sqrt=round(i**0.5)
     for j in range (2,sqrt+1):
         if i%j==0:
-            count+=1
-            spisok.append(j)
+            if i/j!=j:
+                count+=2
+                spisok.append(j)
+                spisok.append(i/j)
+            else:
+                count+=1
+                spisok.append(j)
             if count>2:
                 break
     if count==2:
