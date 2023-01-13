64f5831fd0567508cce18b94b1595e73f17ee907
vitrich <ve@1553ru>
2023-01-13 12:01:28 +0300

    ssss

---
+++ 11/EGE/sofa/26_help.py
@@ -0,0 +1,6 @@
+
+f = open('27883.txt')
+data = f.readlines()
+s = data[0].split()
+s = int(s[0])
+del (data[0])
--- del.py
+++ del.py
@@ -1,3 +1,15 @@
-600
-
-4 200
+def f(x):
+    k=2
+    deliteli=set()
+    while k * k <= x:
+        if x % k==0:
+            deliteli.add(k)
+            if x // k < x:
+                deliteli.add(x // k)
+        k = k + 1
+    return sorted(deliteli)
+start = 174457
+end = 174505
+for i in range(start, end + 1):
+    if len(f(i)) == 2:
+        print(f(i))
