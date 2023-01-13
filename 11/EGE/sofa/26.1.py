0418d29b652e8aa3e048966b0aaebc63caa00a3f
Программист 4 <hkjh@jlkj>
2023-01-13 12:29:29 +0300

    задания

--- 11/EGE/sofa/26.1.py
+++ 11/EGE/sofa/26.1.py
@@ -1,4 +1,23 @@
-f=open('27883.txt')
-s=f.readlines()
-s[0].split
-print(s[0])
+f = open('27883.txt')
+data = f.readlines()
+del (data[0])
+b=2358
+data=[int(i) for i in data]
+data.sort()
+print(data)
+max=data[0]
+maxcount=0
+for i in range (0,b):
+    a=643
+    a-=data[i]
+    count=1
+    for j in range(i+1,b):
+        if a>data[j]:
+            a-=data[j]
+            count+=1
+            if count>maxcount:
+                maxcount=count
+
+
+
+
--- 11/EGE/sofa/8.py
+++ 11/EGE/sofa/8.py
@@ -6,3 +6,11 @@ for h in range(4):
                 k+=1
                 if k==215:
                     print(h,p,t,y)
+k=0
+for h in range(4):
+    for p in range(4):
+        for t in range(4):
+            for y in range (4):
+                k+=1
+                if k==215:
+                    print(h,p,t,y)
