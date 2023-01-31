96f60276624ec11d92e9552c462180fef9d585e7
sonia <dedmasai@gmail.com>
2023-01-31 13:08:40 +0300

    alexander

---
+++ 11/Alexander/ege1/8.py
@@ -0,0 +1,9 @@
+count = 0
+word = "01234567"
+for x in word:
+    for y in word:
+        for z in word:
+            for n in word:
+                for m in word:
+                    a = x+y+z+n+m
+                    if a.count = "4"
