01dd71931e416930dafe834026b7c7cf16ccee60
123 <dd@ddd>
2023-02-21 14:10:56 +0300

    fdhdthth

--- 11/EGE/sofa/2.py
+++ 11/EGE/sofa/2.py
@@ -2,6 +2,6 @@ for x in range (2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                f= x and ((not y) or z) and (y or ((not z)==w))
+                f= not((not y) or (not((not z) or w))) and (z or ((not w)==x))
                 print(x,y,z,w, end='  ')
                 print(f)
