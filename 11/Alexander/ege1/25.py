0f55652298762c7f6ab6b4a340c55ec2a9697143
sonia <dedmasai@gmail.com>
2023-03-28 09:41:19 +0300

    alexander

--- 11/Alexander/ege1/25.py
+++ 11/Alexander/ege1/25.py
@@ -1,11 +1,7 @@
-delit = 123
-for j in range(4):
-    x = int('32'+'0'*j+'823')
-    if x % delit == 0:
-        print(x, end='   ')
-        print(x//delit)
-for i in range(1, 1000):
-    x = int('32'+str(i)+'823')
-    if x % delit == 0:
-        print(x, end='   ')
-        print(x // delit)
+delit = 2079
+for j in range(10):
+    for i in range(1, 1000):
+        x = int('33'+str(i)+'21'+str(j)+'7')
+        if x % delit == 0:
+            print(x, end='   ')
+            print(x//delit)
