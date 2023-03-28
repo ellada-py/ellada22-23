ecfe018496b499ad41815da91506c941b5618c7e
123 <dd@ddd>
2023-03-28 09:41:05 +0300

    fdhdthth

--- 11/EGE/sofa/24.py
+++ 11/EGE/sofa/24.py
@@ -1,12 +1,5 @@
-f=open('24.txt')
+f=open('24var04.txt')
 s=f.read()
-k=1
-max=0
-for i in range(1, len(s)):
-    if s[i] == 'P' and s[i-1] == 'P':
-        k = 1
-    else:
-        k += 1
-        if k > max:
-            max = k
+while 'AB' in s:
+
 print(max)
