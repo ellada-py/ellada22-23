46841968d9e1fb57a455379ffb6278d09d20295a
123 <dd@ddd>
2023-04-26 14:10:29 +0300

    fdhdthth

--- 11/EGE/sofa/17.1.py
+++ 11/EGE/sofa/17.1.py
@@ -3,7 +3,7 @@ s=[int(k) for k in f]
 max=0
 count=0
 for i in range (len(s)-1):
-    for j in range(i-1,len(s)):
+    for j in range(i+1,len(s)):
         if s[i]*s[j]%10==0:
             count+=1
             if s[i]+s[j]>max:
