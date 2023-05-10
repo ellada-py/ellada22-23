c98ba1264a38be7b54bddeb79c00dbedaf74e3dc
123 <dd@ddd>
2023-05-10 13:47:16 +0300

    gdfgdfg

--- 11/EGE/sofa/26.py
+++ 11/EGE/sofa/26.py
@@ -2,8 +2,8 @@ f=open('26.txt')
 s=[int(i) for i in f]
 count=0
 max=0
-for i in range(len(s)):
-    for j in range (i+1,len(s)+1):
+for i in range(len(s)-1):
+    for j in range (i+1,len(s)):
         if (s[i]+s[j])%2==0:
             k=(s[i]+s[j])//2
             if k in s:
