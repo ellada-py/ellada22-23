f44b94bad6f5e0a0454038196ec339084e1e409f
123 <dd@ddd>
2023-05-16 09:43:27 +0300

    gdfgdfg

--- 11/EGE/sofa/2.py
+++ 11/EGE/sofa/2.py
@@ -2,5 +2,5 @@ for x in range (2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if (not(not(x) or w) or (y==z) or y)==0:
+                if (y or (z==w)) and (((not z) or x)==w)==1:
                     print(x,y,z,w)
